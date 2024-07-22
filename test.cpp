fde70ad6d3c57d50c6d8f3d60a3fa5c5a90acda6
A   S   H   I   S   H <96686068+PiPanther@users.noreply.github.com>
2024-07-22 18:54:14 +0530

    Running test.cpp

---
+++ test.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stack>
+#include <stdlib.h>
+
+using namespace std;
+
+struct  Point
+{
+    int x, y;
+};
+
+Point initialPoint;
+
+
+void convexHull('')
+
+
+int main() {
+    Point points[] = {{0, 3}, {1, 1}, {2, 2}, {4, 4},{0, 0}, {1, 2}, {3, 1}, {3, 3}};
+
+    int length = sizeof(points)/sizeof(points[0]);
+
+
+}
